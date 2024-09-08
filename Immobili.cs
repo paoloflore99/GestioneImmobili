65b946f249cdf08c2b5c1623cc10c3b1aa1f8119
paolo flore <paoloflore937@gmail.com>
2024-09-08 14:25:03 +0200

    Aggiungere i file di progetto.

---
+++ Immobili.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestioneImmobili
+{
+    public class Immobili
+    {
+        public string Nome { get; set; }
+        public string Descrizione { get; set; }
+        public int Prezzo { get; set; }
+        public int Iva { get; set; }
+        public Immobili(string nome, string descrizione , int prezzo , int iva)
+        {
+            this.Nome = nome;
+            this.Descrizione = descrizione; 
+            this.Prezzo = prezzo;
+            this.Iva = iva;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿namespace GestioneImmobili
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Immobili> list = new List<Immobili>();
+            Console.WriteLine("nome immobile");
+            string nome = Console.ReadLine();
+            Console.WriteLine("descrizione immobile");
+            string descrizione = Console.ReadLine();
+            Console.WriteLine("prezzo immobile");
+            int prezzo = Convert.ToInt32(Console.ReadLine());
+
+            int calcoloIva(int prezzocasa)
+            {
+                const double fisso = 0.22;
+                int iva = (int)(prezzocasa * fisso);
+                return iva;
+            }
+            int iva = calcoloIva(prezzo);
+            Immobili casauno = new Immobili(nome, descrizione, prezzo, iva);
+            list.Add(casauno);
+            foreach (Immobili i in list)
+            {
+                Console.WriteLine($"{i.Prezzo}  {i.Nome}  { i.Descrizione}  {i.Iva}");
+            }
+
+            Console.WriteLine($"L'iva e di {iva}");
+
+            Console.WriteLine($"quindi in totale e di  {prezzo + iva }");
+        }
+    }
+}
